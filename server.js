90675641baea0c4186dd6ed19217e03335d28686
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2021-08-19 15:34:24 +0400

    last commit

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const database = require('./database/db')
 
 const app = express()
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+)
 app.use(cookieParser())
 app.use(express.json())
 app.use(
@@ -38,7 +42,7 @@ app.use(
 
 const port = process.env.PORT || 5000
 
-//database()
+database()
 
 app.use('/admin', adminRoute)
 app.use('/auth', authRoutes)
@@ -48,7 +52,7 @@ app.use('/api/transactions', transactionRoutes)
 app.use('/api/general', generalRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname + '/client/build')))
+  app.use(express.static(path.join(__dirname + '/client/public')))
   app.get('*', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
