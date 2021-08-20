6c491c98760308f9990201ad0b6e03748af87050
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2021-08-20 12:37:23 +0400

    new commit

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const adminRoute = require('./routes/adminRoute')
 const database = require('./database/db')
 
 const app = express()
-app.use(helmet())
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+)
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -42,23 +46,27 @@ app.use(
 
 const port = process.env.PORT || 5000
 
-//database()
+database()
 
+if (process.env.NODE_ENV === 'production') {
+  // let defaultSrc = "default-src 'none'"
+  // let formSrc = "form-action 'self'"
+  // let scriptSrc = "script-src 'self'"
+  // scriptSrc += 'https://themezhub.net'
+  // let csp = [defaultSrc, formSrc, scriptSrc].join(';')
+  //app.use(expressCspHeader())
+
+  app.use(express.static(path.resolve(__dirname, './client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+}
 app.use('/admin', adminRoute)
 app.use('/auth', authRoutes)
 app.use('/api/notification', notificationRoutes)
 app.use('/api/work', workRoutes)
 app.use('/api/transactions', transactionRoutes)
 app.use('/api/general', generalRoutes)
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, './client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(
-      express.static(path.resolve(__dirname + '/client/build/index.html')),
-    )
-  })
-}
 app.listen(port, () => {
   console.log(`server is running on port ${port} `)
 })
